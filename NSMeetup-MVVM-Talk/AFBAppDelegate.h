e938b5b6e697913b1ec32d1407bd12c393cfbe29
Adam Block <adam@usa.net>
2014-05-07 09:07:39 -0700

    Initial Commit

---
+++ NSMeetup-MVVM-Talk/AFBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AFBAppDelegate.h
+//  NSMeetup-MVVM-Talk
+//
+//  Created by Adam Block on 5/7/14.
+//  Copyright (c) 2014 Adam Block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSMeetup-MVVM-Talk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSMeetup-MVVM-Talk
+//
+//  Created by Adam Block on 5/7/14.
+//  Copyright (c) 2014 Adam Block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AFBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AFBAppDelegate class]));
+    }
+}
